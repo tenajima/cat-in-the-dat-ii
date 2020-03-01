a60ffdea761a9cbc7810352f2719d8773410b57b
Yuto <tenajima@gmail.com>
2020-03-01 21:41:44 +0900

    diff, devide featureの実装 (#20)

--- experiments/preprocess.py
+++ experiments/preprocess.py
@@ -7,6 +7,3 @@ from scripts.submit.predict import Predict
 if __name__ == "__main__":
     load_dotenv()
     luigi.run(["Preprocess", "--workers", "8", "--local-scheduler"])
-    # luigi.run(["TuningLGB",  "--local-scheduler"])
-    # luigi.run(["TrainStratifiedKFold",  "--local-scheduler"])
-    # luigi.run(["Predict",  "--local-scheduler"])
